edf787ac95c1157f19a046b9d4e1724b701a11d7
Nick <a-nickshieh@microsoft.com>
2023-12-08 00:42:03 +0800

    update choose prompt type in target_config.json

--- app.py
+++ app.py
@@ -12,19 +12,14 @@ from spyder import *
 
 load_dotenv()  
   
-def load_system_prompts():  
+def load_system_prompts(target):  
     """
-    讀取prompts.toml中的system prompt，若要使用其他版本的prompt請在此處選擇v1、v2、v3....
+    讀取prompts.toml中的system prompt，若要使用其他版本的prompt請在target_config.json選擇v1、v2、v3....
     更改prompt請依照順序v1->v2->v3，請勿直接更改現有版本！！！
     """
     with open('prompts.toml', 'r') as f:  
-        data = toml.load(f)  
-    return {  
-        "GPT_SUMMARY_PROMPT": data['gpt_summary_prompt_v2']['prompt'],  
-        "GPT_TITLE_PROMPT": data['gpt_title_prompt_v3']['prompt'],  
-        "GPT_SIMILARITY_PROMPT": data['gpt_similarity_prompt_v1']['prompt'],  
-        "GPT_WEEKLY_SUMMARY_PROMPT": data['gpt_weekly_summary_prompt_v1']['prompt']  
-    }  
+        data = toml.load(f)
+        return {k: v for k, v in target.items() if "GPT" in k}
   
 def load_targets_config(): 
     """
@@ -33,7 +28,7 @@ def load_targets_config():
     with open('target_config.json', 'r') as f:  
         return json.load(f)  
   
-def process_targets(targets, system_prompts):
+def process_targets(targets):
     """
     根據target_config.json的目標依次爬取更新並總結推送至teams的channel
     並在每週一推送一次上週更新總結
@@ -42,8 +37,9 @@ def process_targets(targets, system_prompts):
         topic = target['topic_name']  
         root_commits_url = target['root_commits_url']  
         language = target['language']  
-        teams_webhook_url = target['teams_webhook_url']  
-  
+        teams_webhook_url = target['teams_webhook_url']
+        system_prompts = load_system_prompts(target)
+
         logger.warning(f"========================= Start to process topic: {topic} =========================")  
         logger.info(f"Root commits url: {root_commits_url}")  
         logger.info(f"Language: {language}")  
@@ -59,7 +55,7 @@ def process_targets(targets, system_prompts):
         now = datetime.datetime.now()
         seconds_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()    
         if (now.weekday() == 0 and seconds_since_midnight < git_spyder.schedule) or this_week_summary is None:  
-            git_spyder.generate_weekly_summary()  
+            git_spyder.generate_weekly_summary()
         logger.warning(f"Finish processing topic: {topic}")  
     return git_spyder.schedule
 
@@ -67,12 +63,11 @@ def main():
     """
     以循環方式固定時間爬取一次檢測是否有文檔更新
     """
-    system_prompts = load_system_prompts()  
     targets = load_targets_config()  
   
     while True:  
         try:  
-            schedule = process_targets(targets, system_prompts)  
+            schedule = process_targets(targets)  
             logger.warning(f"Waiting for {schedule} seconds")  
             time.sleep(schedule)  
         except Exception as e:  
